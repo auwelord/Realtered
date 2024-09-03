6dca0d262f9d091345cc142c0579880a376e4472
auwelord <apalancher@gmail.com>
2024-09-03 16:49:42 +0200

    feat: env file

--- src/db/client.js
+++ src/db/client.js
@@ -1,4 +1,4 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://fyqptmokmnymednlerpj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5cXB0bW9rbW55bWVkbmxlcnBqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyNjMzMjcsImV4cCI6MjA0MDgzOTMyN30.tl3fXrsJ3tstifH5t5JEXQdbD2xSYcut7xbKrnmZFG0')
+export const supabase = createClient(import.meta.env.VITE_SUPABASE_CLIENT_URL, import.meta.env.VITE_SUPABASE_CLIENT_SECRET)
