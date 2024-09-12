6554ff36e8eb1264517a08167d60b94229fa4a4d
auwelord <apalancher@gmail.com>
2024-09-12 18:07:00 +0200

    fix: createbrowserclient

--- src/assets/js/supabase.js
+++ src/assets/js/supabase.js
@@ -1,26 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
-import Cookies from 'js-cookie';
+import { createBrowserClient } from '@supabase/ssr'
 
 export function anonCreateClient(){
-    return createClient(import.meta.env.VITE_SUPABASE_CLIENT_URL, import.meta.env.VITE_SUPABASE_CLIENT_ANONKEY,  
-    {
-        auth: {
-            //detectSessionInUrl: true,
-            storage:  {
-                getItem: (key) => {
-                    //console.log('getItem:' + key)
-                    return Cookies.get(key)
-                },
-                setItem: (key, value) => {
-                    //console.log('setItem:' + key)
-                    //console.log(value)
-                    return Cookies.set(key, value)
-                },
-                removeItem: (key) => {
-                    //console.log('removeItem:' + key)
-                    return Cookies.remove(key)
-                },
-            }
-        }
-    })
+    return createBrowserClient(import.meta.env.VITE_SUPABASE_CLIENT_URL, import.meta.env.VITE_SUPABASE_CLIENT_ANONKEY)
 }
